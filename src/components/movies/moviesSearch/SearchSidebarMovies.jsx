4706591c02be3f3eaa04efddd9749637b1432a5f
petaccia <petaccia.seb.dev@gmail.com>
2024-12-03 13:02:41 +0100

    feat(layout): Optimisation espacement SearchSidebarMovies
    - Ajout margin-top 5rem
    - Amélioration lisibilité titre principal
    - Meilleure séparation avec la navbar

--- src/components/movies/moviesSearch/SearchSidebarMovies.jsx
+++ src/components/movies/moviesSearch/SearchSidebarMovies.jsx
@@ -1,22 +1,20 @@
 "use client"
 import React from 'react'
 import { useSelectedLayoutSegment, useParams } from 'next/navigation'
-import Custom404 from '@/app/not-found';
-import FormsearchSidebarMovies from './formSearchSidebarMovies/FormsearchSidebarMovies';
+import Custom404 from '@/app/not-found'
+import FormsearchSidebarMovies from './formSearchSidebarMovies/FormsearchSidebarMovies'
 
 const SearchSidebarMovies = ({ genres }) => {
     const segment = useSelectedLayoutSegment();
     const { id } = useParams();
    
-    // Fonction pour le titre des films
     const getSidebarTitle = () => {
         if (!segment) {
-          return 'Tous les films';
+            return 'Tous les films';
         }
 
-        const genre = genres.find((genre) => genre.id === Number(id));
+        const genre = genres?.find((genre) => genre.id === Number(id));
         if (!genre) {
-            // retourner la page 404
             return Custom404;
         }
 
@@ -24,20 +22,27 @@ const SearchSidebarMovies = ({ genres }) => {
     };
 
     const title = getSidebarTitle();
+    
+    if (title === Custom404) {
+        return <Custom404 />;
+    }
 
-  return (
-    <div className="max-w-screen-md mx-auto">
-        <header className="mb-6">
-            <h1 className="text-2xl lg:text-3xl font-bold text-text-primary mb-6 text-center">Tous les films d' 
-                <span className="text-text-highlight"> {title} </span>
-            </h1>
-        </header>
-        <section>
-            <FormsearchSidebarMovies />
-        </section>
-
-    </div>
-  )
+    return (
+        <div className="max-w-screen-md mx-auto mt-16">
+            <header className="mb-6">
+                <h1 className="text-2xl lg:text-3xl font-bold text-text-primary mb-6 text-center">
+                    Films de la catégorie{' '}
+                    <span className="text-text-highlight">
+                        {title}
+                    </span>
+                </h1>
+            </header>
+            
+            <section>
+                <FormsearchSidebarMovies />
+            </section>
+        </div>
+    )
 }
 
 export default SearchSidebarMovies
